b5043c04ad7f5997ab76ed4228bb10af092a65b5
aidan-walden <44129920+aidan-walden@users.noreply.github.com>
2019-07-03 15:48:39 -0400

    Created objects for managing player in new SteamVR version

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Valve.VR.InteractionSystem;
+using Valve.VR.Extras;
+using Valve.VR;
 
 public class MainMenu : MonoBehaviour
 {
     public void loadFacility()
     {
-        SceneManager.LoadScene(1);
+        Transform SteamObjects = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0);
+        Transform[] hands = { SteamObjects.GetChild(1), SteamObjects.GetChild(2) };
+        foreach(Transform hand in hands) //Remove UI Laser interaction scripts
+        {
+            Destroy(hand.gameObject.GetComponent<VRUIInput>());
+            Destroy(hand.gameObject.GetComponent<SteamVR_LaserPointer>());
+        }
+        GetComponent<SteamVR_LoadLevel>().Trigger();
     }
 
     public void quitGame()
